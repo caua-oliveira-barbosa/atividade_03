8314f1f4784ecd785fb313ea787f8a70a35b1e39
Cauã <cauaoliveirabarbosa7@gmail.com>
2024-08-16 11:13:33 -0300

    Adicionei o before each e criei uma verificação do h1 pela classe titulo principal

--- frontend/cypress/e2e/spec.cy.js
+++ frontend/cypress/e2e/spec.cy.js
@@ -1,6 +1,10 @@
 describe('pagina inicial', () => {
+  beforeEach( () => {
+    cy.visit('http://localhost:3000/')
+  })
   it('Deve renderizar h1 com o texto correto!', () => {
-    cy.visit('http://localhost:3000')
-    cy.get('[data-test="titulo-principal"]').contains('Experimente mais liberdade no controle de sua vida financeira. Crie sua conta com a gente!')
+    // cy.visit('http://localhost:3000')
+    cy.getByData('titulo-principal').contains('Experimente mais liberdade no controle da sua vida financeira. Crie sua conta com a gente!')
+    
   })
 })
