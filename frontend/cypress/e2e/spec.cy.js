4b373b6f0c3571cd254eb9c160a63e3bc56aadf5
Cauã <cauaoliveirabarbosa7@gmail.com>
2024-08-16 14:50:19 -0300

    Verifiquei todos os elementos da pagina

--- frontend/cypress/e2e/spec.cy.js
+++ frontend/cypress/e2e/spec.cy.js
@@ -4,7 +4,26 @@ describe('pagina inicial', () => {
   })
   it('Deve renderizar h1 com o texto correto!', () => {
     // cy.visit('http://localhost:3000')
-    cy.getByData('titulo-principal').contains('Experimente mais liberdade no controle da sua vida financeira. Crie sua conta com a gente!')
+    cy.get('#main3')
     
+  })
+  it('Deve renderizar h2 com o texto correto!', () => {
+    cy.get('#main4')
+  })
+
+  it('Deve renderizar h3 com o texto correto!', () => {
+    cy.get('#main5')
+  })
+
+  it('Deve renderizar p com o texto correto!', () => {
+    cy.get('#main6')
+  })
+
+  it('Deve renderizar as divs com sucesso', () => {
+    cy.get('#main')
+  })
+  
+  it('Deve renderizar as imagensn  com sucesso', () => {
+    cy.get('#main2')
   })
 })
--- frontend/src/paginas/Inicio/index.jsx
+++ frontend/src/paginas/Inicio/index.jsx
@@ -9,55 +9,75 @@ import dispositivo from 'assets/dispositivo.svg';
 export default function Inicio() {
   return (
     <>
-      <div className={estilos.container}>
-        <div className={estilos.conteudo}>
-          <div className={estilos.banner}>
+      <div id='main' className={estilos.container}>
+        <div id='main' className={estilos.conteudo}>
+          <div id='main' className={estilos.banner}>
+            <div id='main3' className='texto'>
             <h1 data-test="titulo-principal">
               Experimente mais liberdade no controle da sua vida financeira.
               Crie sua conta com a gente!
             </h1>
-            <img
+            </div>
+            <img id='main2'
               src={ilustracaoBanner}
               alt="Gráficos e uma pessoa com dinheiro na mão"
             />
           </div>
-          <div className={estilos.vantagens}>
-            <h2>Vantagens do nosso banco:</h2>
-            <div className={estilos.vantagens__itens}>
-              <div className={estilos.vantagens__item}>
-                <img src={presente} alt="ícone de um presente" />
-                <h3>Conta e cartão gratuitos</h3>
-                <p>
+          <div id='main' className={estilos.vantagens}>
+            <div id='main4'>
+            <h2 data-test="segundo-titulo">Vantagens do nosso banco:</h2>
+            </div>
+            <div id='main' className={estilos.vantagens__itens}>
+              <div id='main' className={estilos.vantagens__item}>
+                <img id='main2' src={presente} alt="ícone de um presente" />
+                <div id='main5'>
+                <h3 data-test="terceiro-titulo">Conta e cartão gratuitos</h3>
+                </div>
+                <div id='main6'>
+                <p data-test="paragrafo">
                   Isso mesmo, nossa conta é digital, sem custo fixo e mais que
                   isso: sem tarifa de manutenção.
                 </p>
+                </div>
               </div>
-              <div className={estilos.vantagens__item}>
-                <img src={saque} alt="ícone de uma mão segurando um dinheiro" />
-                <h3>Saques sem custo</h3>
-                <p>
+              <div id='main' className={estilos.vantagens__item}>
+                <img id='main2' src={saque} alt="ícone de uma mão segurando um dinheiro" />
+                <div id='main4'>
+                <h3 data-test="terceiro-titulo">Saques sem custo</h3>
+                </div>
+                <div id='main6'>
+                <p data-test='paragrafo'>
                   Você pode sacar gratuitamente 4x por mês de qualquer Banco
                   24h.
                 </p>
+                </div>
               </div>
-              <div className={estilos.vantagens__item}>
-                <img src={estrela} alt="ícone de uma estrela" />
-                <h3>Programa de pontos</h3>
+              <div id='main' className={estilos.vantagens__item}>
+                <img id='main2' src={estrela} alt="ícone de uma estrela" />
+                <div id='main5'>
+                <h3 data-test="terceiro-titulo">Programa de pontos</h3>
+                </div>
+                <div id='main6'>
                 <p>
                   Você pode acumular pontos com suas compras no crédito sem
                   pagar mensalidade!
                 </p>
+                </div>
               </div>
-              <div className={estilos.vantagens__item}>
+              <div id='main' className={estilos.vantagens__item}>
                 <img
                   src={dispositivo}
                   alt="ícone de um dispositivo como notebook"
                 />
+                <div id='main5'>
                 <h3>Seguro Dispositivos</h3>
+                </div>
+                <div id='main6'>
                 <p>
                   Seus dispositivos móveis (computador e laptop) protegidos por
                   uma mensalidade simbólica.
                 </p>
+                </div>
               </div>
             </div>
           </div>
