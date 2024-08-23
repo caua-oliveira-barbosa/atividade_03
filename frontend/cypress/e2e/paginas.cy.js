eefb519778512fc1155c1dcb086f3f464560bb9b
Cauã <cauaoliveirabarbosa7@gmail.com>
2024-08-23 14:32:11 -0300

    Verificação de um elemento de todas as páginas

--- frontend/cypress/e2e/paginas.cy.js
+++ frontend/cypress/e2e/paginas.cy.js
@@ -8,4 +8,51 @@ describe('Testando múltiplas páginas', () => {
     cy.getByData('app-home').find('a').eq(1).click()
     cy.getByData('titulo-cartoes').should('exist').and('have.text', 'Meus cartões')
     })
-});
+
+    it('Deve conseguir acessar a página de cartões', () => {
+        cy.visit('http://localhost:3000')
+        cy.getByData('botao-login').click()
+        cy.getByData('email-input').type('neilton@alura.com')
+        cy.getByData('senha-input').type('123456')
+        cy.getByData('botao-enviar').click()
+        /*Adicione a seguinte linha no seu código:*/
+        cy.location('pathname').should('eq','/home')
+        cy.getByData('app-home').find('a').eq(1).click()
+        cy.getByData('titulo-cartoes').should('exist').and('have.text', 'Meus cartões')
+        /*Adicione a seguinte linha no seu código:*/
+        cy.location('pathname').should('eq','/home/cartoes')
+
+    });
+
+    it('Deve conseguir acessar a página de servicos', () => {
+        cy.visit('http://localhost:3000')
+        cy.getByData('botao-login').click()
+        cy.getByData('email-input').type('neilton@alura.com')
+        cy.getByData('senha-input').type('123456')
+        cy.getByData('botao-enviar').click()
+        /*Adicione a seguinte linha no seu código:*/
+        cy.location('pathname').should('eq','/home')
+        cy.getByData('app-home').find('a').eq(2).click()
+        
+        /*Adicione a seguinte linha no seu código:*/
+        cy.location('pathname').should('eq','/home/servicos')
+
+      
+    });
+
+    it('Deve conseguir acessar a página de cartões', () => {
+        cy.visit('http://localhost:3000')
+        cy.getByData('botao-login').click()
+        cy.getByData('email-input').type('neilton@alura.com')
+        cy.getByData('senha-input').type('123456')
+        cy.getByData('botao-enviar').click()
+        /*Adicione a seguinte linha no seu código:*/
+        cy.location('pathname').should('eq','/home')
+        cy.getByData('app-home').find('a').eq(3).click()
+        
+        /*Adicione a seguinte linha no seu código:*/
+        cy.location('pathname').should('eq','/home/investimentos')
+
+    });
+    
+})
